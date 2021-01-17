8e947b29a9aff949acd70568a06643b0db712cbd
PepegaCoder <56232333+PepegaCoder@users.noreply.github.com>
2021-01-16 23:30:52 -0700

    checkPrice function created
    function to check the price of items

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ items = [] #Enter items in a list with the original price, example: items = [["h
 #Enter in your user agent by going to google and searching: "my user agent." Replace your user agent after the "User-Agent":
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
+
+def checkPrice():
+    #function that will check the price of the items in the items list
+    page = requests.get(URL, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find(id="productTitle").get_text()
+    print("Checking price for " + title.strip())
+    price = soup.find(id="priceblock_ourprice").get_text()
+
